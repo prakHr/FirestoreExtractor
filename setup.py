1632b7e21707b1c94ce73c6ce4f9b0bee453dfdd
monkeyD <gprakhar0@gmail.com>
2021-07-24 00:41:21 +0530

    updated v4

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'FirestoreExtractor',         # How you named your package folder (MyLib)
-  packages = find_packages(exclude=['test']),   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  packages = find_packages('FirestoreExtractor'),   # Chose the same as "name"
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Extracts data from cloud firestore and firebase database',   # Give a short description about your library
   author = 'Prakhar Gandhi',                   # Type in your name
@@ -14,7 +14,6 @@ setup(
   keywords = ['cloud-firestore', 'firebase', 'firestore'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'firebase-admin',
-          'FirestoreExtractor',
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
